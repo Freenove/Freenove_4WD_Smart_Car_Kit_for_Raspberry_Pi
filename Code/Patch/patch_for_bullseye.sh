1058608b0cd3b9ecd397f34b65ecc215a843a981
DenzelChen <291689742@qq.com>
2021-11-15 14:35:03 +0800

    Update camera patch for new OS

---
+++ Code/Patch/patch_for_bullseye.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+current_file=$0
+cd "$(dirname "$current_file")"
+
+sudo cp /boot/config.txt /boot/config-backup.txt
+sudo cp ./config.txt /boot/config.txt
+sudo cp /usr/lib/arm-linux-gnueabihf/libmmal.so /usr/lib/arm-linux-gnueabihf/libmmal-backup.so.backup
+sudo cp ./libmmal.so /usr/lib/arm-linux-gnueabihf/libmmal.so
+
+echo "patched complete! Please reboot the system!"
+
