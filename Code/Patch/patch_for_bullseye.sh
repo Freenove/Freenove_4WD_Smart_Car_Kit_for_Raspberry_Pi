d6d912f9c71917e1f815434f69ba07ddbe42e080
DenzelChen <291689742@qq.com>
2021-11-16 17:12:51 +0800

    Update patch and tutorial

--- Code/Patch/patch_for_bullseye.sh
+++ Code/Patch/patch_for_bullseye.sh
@@ -2,8 +2,8 @@
 current_file=$0
 cd "$(dirname "$current_file")"
 
-sudo cp /boot/config.txt /boot/config-backup.txt
-sudo cp ./config.txt /boot/config.txt
+#sudo cp /boot/config.txt /boot/config-backup.txt
+#sudo cp ./config.txt /boot/config.txt
 sudo cp /usr/lib/arm-linux-gnueabihf/libmmal.so /usr/lib/arm-linux-gnueabihf/libmmal-backup.so.backup
 sudo cp ./libmmal.so /usr/lib/arm-linux-gnueabihf/libmmal.so
 
