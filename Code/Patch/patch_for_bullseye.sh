b62cc3d47e6dda516bebffb856698a6391574a00
DenzelChen <291689742@qq.com>
2021-11-22 10:33:02 +0800

    Update patch_for_bullseye.sh

--- Code/Patch/patch_for_bullseye.sh
+++ Code/Patch/patch_for_bullseye.sh
@@ -4,8 +4,12 @@ cd "$(dirname "$current_file")"
 
 #sudo cp /boot/config.txt /boot/config-backup.txt
 #sudo cp ./config.txt /boot/config.txt
+
 sudo cp /usr/lib/arm-linux-gnueabihf/libmmal.so /usr/lib/arm-linux-gnueabihf/libmmal-backup.so.backup
 sudo cp ./libmmal.so /usr/lib/arm-linux-gnueabihf/libmmal.so
 
-echo "patched complete! Please reboot the system!"
+sudo cp /opt/vc/lib/libmmal.so /opt/vc/lib/libmmal.so.bak 
+sudo cp ./libmmal.so /opt/vc/lib/libmmal.so
+
+echo "patched complete!"
 
