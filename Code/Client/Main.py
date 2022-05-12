42f9f0cda58242bb11235338746cc64a22e167e3
psh4nk <psh4nk@outlook.com>
2022-05-12 10:32:15 -0400

    object detection WIP

--- Code/Client/Main.py
+++ Code/Client/Main.py
@@ -18,7 +18,8 @@ from Video import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import * 
+from PyQt5.QtGui import *
+
 class mywindow(QMainWindow,Ui_Client):
     def __init__(self):
         global timer
--- Code/Client/Video.py
+++ Code/Client/Video.py
@@ -44,111 +44,111 @@ class VideoStreaming:
     def face_detect(self,img):
         if sys.platform.startswith('win') or sys.platform.startswith('darwin'):
 
-            #gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-            #faces = self.face_cascade.detectMultiScale(gray,1.3,5)
+            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            faces = self.face_cascade.detectMultiScale(gray,1.3,5)
 
-            MODEL_NAME = 'Sample_TFLite_model'
-            GRAPH_NAME = 'detect.tflite'
-            LABELMAP_NAME = 'labelmap.txt'
+        #    MODEL_NAME = 'Sample_TFLite_model'
+        #    GRAPH_NAME = 'detect.tflite'
+        #    LABELMAP_NAME = 'labelmap.txt'
 
-            CWD_PATH = os.getcwd()
+        #    CWD_PATH = os.getcwd()
 
-            PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
+        #    PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
 
-            PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
+        #    PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
 
-            with open(PATH_TO_LABELS, 'r') as f:
-                labels = [line.strip() for line in f.readlines()]
+        #    with open(PATH_TO_LABELS, 'r') as f:
+        #        labels = [line.strip() for line in f.readlines()]
 
-            # Have to do a weird fix for label map if using the COCO "starter model" from
-            # https://www.tensorflow.org/lite/models/object_detection/overview
-            # First label is '???', which has to be removed.
-            if labels[0] == '???':
-                del(labels[0])
+        #    # Have to do a weird fix for label map if using the COCO "starter model" from
+        #    # https://www.tensorflow.org/lite/models/object_detection/overview
+        #    # First label is '???', which has to be removed.
+        #    if labels[0] == '???':
+        #        del(labels[0])
 
-            interpreter = Interpreter(model_path=PATH_TO_CKPT)
+        #    interpreter = Interpreter(model_path=PATH_TO_CKPT)
 
-            interpreter.allocate_tensors()
+        #    interpreter.allocate_tensors()
 
-            # Get model details
-            input_details = interpreter.get_input_details()
-            output_details = interpreter.get_output_details()
-            height = input_details[0]['shape'][1]
-            width = input_details[0]['shape'][2]
+        #    # Get model details
+        #    input_details = interpreter.get_input_details()
+        #    output_details = interpreter.get_output_details()
+        #    height = input_details[0]['shape'][1]
+        #    width = input_details[0]['shape'][2]
 
-            floating_model = (input_details[0]['dtype'] == np.float32)
+        #    floating_model = (input_details[0]['dtype'] == np.float32)
 
-            input_mean = 127.5
-            input_std = 127.5
+        #    input_mean = 127.5
+        #    input_std = 127.5
 
-            # Check output layer name to determine if this model was created with TF2 or TF1,
-            # because outputs are ordered differently for TF2 and TF1 models
-            outname = output_details[0]['name']
+        #    # Check output layer name to determine if this model was created with TF2 or TF1,
+        #    # because outputs are ordered differently for TF2 and TF1 models
+        #    outname = output_details[0]['name']
 
-            if ('StatefulPartitionedCall' in outname): # This is a TF2 model
-                boxes_idx, classes_idx, scores_idx = 1, 3, 0
-            else: # This is a TF1 model
-                boxes_idx, classes_idx, scores_idx = 0, 1, 2
+        #    if ('StatefulPartitionedCall' in outname): # This is a TF2 model
+        #        boxes_idx, classes_idx, scores_idx = 1, 3, 0
+        #    else: # This is a TF1 model
+        #        boxes_idx, classes_idx, scores_idx = 0, 1, 2
 
-            # Initialize frame rate calculation
-            frame_rate_calc = 1
-            freq = cv2.getTickFrequency()
+        #    # Initialize frame rate calculation
+        #    frame_rate_calc = 1
+        #    freq = cv2.getTickFrequency()
 
-            frame_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        #    frame_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-            frame_resized = cv2.resize(frame_rgb, (width, height))
-            input_data = np.expand_dims(frame_resized, axis=0)
+        #    frame_resized = cv2.resize(frame_rgb, (width, height))
+        #    input_data = np.expand_dims(frame_resized, axis=0)
 
-            # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-            if floating_model:
-                input_data = (np.float32(input_data) - input_mean) / input_std
+        #    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+        #    if floating_model:
+        #        input_data = (np.float32(input_data) - input_mean) / input_std
 
-            # Perform the actual detection by running the model with the image as input
-            interpreter.set_tensor(input_details[0]['index'],input_data)
-            interpreter.invoke()
+        #    # Perform the actual detection by running the model with the image as input
+        #    interpreter.set_tensor(input_details[0]['index'],input_data)
+        #    interpreter.invoke()
 
-            # Retrieve detection results
-            boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
-            classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
-            scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
+        #    # Retrieve detection results
+        #    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
+        #    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
+        #    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
 
-            # Loop over all detections and draw detection box if confidence is above minimum threshold
-            for i in range(len(scores)):
-                if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+        #    # Loop over all detections and draw detection box if confidence is above minimum threshold
+        #    for i in range(len(scores)):
+        #        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
 
-                    # Get bounding box coordinates and draw box
-                    # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-                    ymin = int(max(1,(boxes[i][0] * imH)))
-                    xmin = int(max(1,(boxes[i][1] * imW)))
-                    ymax = int(min(imH,(boxes[i][2] * imH)))
-                    xmax = int(min(imW,(boxes[i][3] * imW)))
+        #            # Get bounding box coordinates and draw box
+        #            # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+        #            ymin = int(max(1,(boxes[i][0] * imH)))
+        #            xmin = int(max(1,(boxes[i][1] * imW)))
+        #            ymax = int(min(imH,(boxes[i][2] * imH)))
+        #            xmax = int(min(imW,(boxes[i][3] * imW)))
 
-                    cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+        #            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
 
-                    # Draw label
-                    object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
-                    if (object_name == "person"):
-                        label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
-                        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
-                        label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
-                        cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
-                        cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+        #            # Draw label
+        #            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+        #            if (object_name == "person"):
+        #                label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+        #                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+        #                label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+        #                cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+        #                cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
 
-            # Draw framerate in corner of frame
-            cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
+        #    # Draw framerate in corner of frame
+        #    cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
 
-        cv2.imwrite('video.jpg', frame)
+        #cv2.imwrite('video.jpg', frame)
 
             # TODO Replace the two calls above with calls to VideoStream.process()
-            #if len(faces)>0 :
-            #    for (x,y,w,h) in faces:
-            #        self.face_x=float(x+w/2.0)
-            #        self.face_y=float(y+h/2.0)
-            #        img= cv2.circle(img, (int(self.face_x),int(self.face_y)), int((w+h)/4), (0, 255, 0), 2)
-            #else:
-            #    self.face_x=0
-            #    self.face_y=0
-        #cv2.imwrite('video.jpg',img)
+            if len(faces)>0 :
+                for (x,y,w,h) in faces:
+                    self.face_x=float(x+w/2.0)
+                    self.face_y=float(y+h/2.0)
+                    img= cv2.circle(img, (int(self.face_x),int(self.face_y)), int((w+h)/4), (0, 255, 0), 2)
+            else:
+                self.face_x=0
+                self.face_y=0
+        cv2.imwrite('video.jpg',img)
         
     def streaming(self,ip):
         stream_bytes = b' '
