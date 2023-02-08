28a6f09c11dcde1dba256ff536098fa7318a9f25
PJB123456 <55983097+PJB123456@users.noreply.github.com>
2023-02-08 13:51:11 +0800

    Update Ultrasonic.py

--- Code/Server/Ultrasonic.py
+++ Code/Server/Ultrasonic.py
@@ -4,35 +4,39 @@ import RPi.GPIO as GPIO
 from servo import *
 from PCA9685 import PCA9685
 class Ultrasonic:
-    def __init__(self):
-        GPIO.setwarnings(False)
+    def __init__(self):        
+        GPIO.setwarnings(False)        
         self.trigger_pin = 27
         self.echo_pin = 22
+        self.MAX_DISTANCE = 300               # define the maximum measuring distance, unit: cm
+        self.timeOut = self.MAX_DISTANCE*60   # calculate timeout according to the maximum measuring distance
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.trigger_pin,GPIO.OUT)
         GPIO.setup(self.echo_pin,GPIO.IN)
-    def send_trigger_pulse(self):
-        GPIO.output(self.trigger_pin,True)
-        time.sleep(0.00015)
-        GPIO.output(self.trigger_pin,False)
-
-    def wait_for_echo(self,value,timeout):
-        count = timeout
-        while GPIO.input(self.echo_pin) != value and count>0:
-            count = count-1
-     
-    def get_distance(self):
+        
+    def pulseIn(self,pin,level,timeOut): # obtain pulse time of a pin under timeOut
+        t0 = time.time()
+        while(GPIO.input(pin) != level):
+            if((time.time() - t0) > timeOut*0.000001):
+                return 0;
+        t0 = time.time()
+        while(GPIO.input(pin) == level):
+            if((time.time() - t0) > timeOut*0.000001):
+                return 0;
+        pulseTime = (time.time() - t0)*1000000
+        return pulseTime
+    
+    def get_distance(self):     # get the measurement results of ultrasonic module,with unit: cm
         distance_cm=[0,0,0,0,0]
-        for i in range(3):
-            self.send_trigger_pulse()
-            self.wait_for_echo(True,10000)
-            start = time.time()
-            self.wait_for_echo(False,10000)
-            finish = time.time()
-            pulse_len = finish-start
-            distance_cm[i] = pulse_len/0.000058
+        for i in range(5):
+            GPIO.output(self.trigger_pin,GPIO.HIGH)      # make trigger_pin output 10us HIGH level 
+            time.sleep(0.00001)     # 10us
+            GPIO.output(self.trigger_pin,GPIO.LOW) # make trigger_pin output LOW level 
+            pingTime = self.pulseIn(self.echo_pin,GPIO.HIGH,self.timeOut)   # read plus time of echo_pin
+            distance_cm[i] = pingTime * 340.0 / 2.0 / 10000.0     # calculate distance with sound speed 340m/s
         distance_cm=sorted(distance_cm)
-        return int(distance_cm[2])
+        return  int(distance_cm[2])
+    
     def run_motor(self,L,M,R):
         if (L < 30 and M < 30 and R <30) or M < 30 :
             self.PWM.setMotorModel(-1450,-1450,-1450,-1450) 
