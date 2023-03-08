2d9dcef90012092359a53f1abe7a44a0de1ed0c6
PJB123456 <55983097+PJB123456@users.noreply.github.com>
2023-03-08 17:42:59 +0800

    Update Led.py

--- Code/Server/Led.py
+++ Code/Server/Led.py
@@ -5,6 +5,8 @@ from rpi_ws281x import *
 LED_COUNT      = 8      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+#When the LED cannot display normally, change the LED signal frequency to 110khz and try again
+#LED_FREQ_HZ    = 1100000  # LED signal frequency in hertz (usually 110khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
