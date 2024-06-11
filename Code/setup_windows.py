bfc3aacc0ea92a8d6ded0cd617a4107b1cd1812d
PJB123456 <55983097+PJB123456@users.noreply.github.com>
2024-06-11 14:00:26 +0800

    Update setup_windows.py

--- Code/setup_windows.py
+++ Code/setup_windows.py
@@ -1,31 +1,35 @@
 import os
 import sys
 import time
-os.system("cd /usr/bin && sudo rm python && sudo ln -s python3 python")
 flag=0x00
 for x in range(1,4):
-    if os.system("sudo apt-get update") == 0:
-        flag=flag | 0x01
-        break
-        
+	if os.system("python -m pip install --upgrade pip --user") == 0:
+		flag=flag | 0x01
+		break
 for x in range(1,4):
-    if os.system("cd ./Libs/rpi-ws281x-python/library && sudo python3 setup.py install") == 0:
-        flag=flag | 0x02
-        break
-
+	if os.system("pip3 install PyQt5") == 0:
+		flag=flag | 0x02
+		break
 for x in range(1,4):
-    if os.system("sudo apt-get install -y python3-dev python3-pyqt5 ") == 0:
-        flag=flag | 0x04
-        break
-        
-if flag==0x07:
-    print("\nNow the installation is successful.")
+	if os.system("pip3 install Pillow") == 0:
+		flag=flag | 0x04
+		break
+for x in range(1,4):
+	if os.system("pip3 install opencv-python") == 0:
+		if os.system("pip3 install opencv-contrib-python") == 0:
+			flag=flag | 0x08
+			break
+for x in range(1,4):
+	if os.system("pip3 install numpy") == 0:
+		flag=flag | 0x10
+		break
+if flag==0x1f:
+		os.system("pip3 list && pause")
+		print("\nAll libraries installed successfully")
 else:
-    if flag&0x01==0x00:
-        print("\napt-get update failed.")
-    if flag&0x02==0x00:
-        print("\nrpi-ws281x-python install failed.")
-    if flag&0x04==0x00:
-        print("\nlibqt5gui5 python3-dev python3-pyqt5 install failed.")
-    print ("\nSome libraries have not been installed yet. Please run 'sudo python setup.py' again")
+		print ("\nSome libraries have not been installed yet. Please run 'sudo python setup_windows.py' again")
+
+	
+	
+
 
