7eb189d1973e172916fe77c9f3eb634515d43d5b
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2025-01-26 21:59:13 -0600

    kill all function

---
+++ Code/Server/Kill.py
@@ -0,0 +1,17 @@
+import time
+from Motor import *
+from gpiozero import DistanceSensor
+from servo import *
+from PCA9685 import PCA9685
+import random
+import move
+import sys
+
+   
+PWM = Motor()
+
+PWM.setMotorModel(0, 0, 0, 0)
+
+servo = Servo()
+servo.setServoPwm('0', 0)
+servo.setServoPwm('0', 1)
