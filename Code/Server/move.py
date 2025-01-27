041ba6192874fbe1b60a899532c829dc879b9c7e
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2025-01-26 21:40:47 -0600

    sleep time and back

--- Code/Server/move.py
+++ Code/Server/move.py
@@ -5,6 +5,7 @@ from servo import *
 from PCA9685 import PCA9685
 import random
 
+SLEEP_TIME = 0.5
 
 class Move:
     def __init__(self):        
@@ -12,19 +13,19 @@ class Move:
     
     def back(self):
         self.PWM.setMotorModel(-600,-600,-600,-600)
-        time.sleep(1)
+        time.sleep(SLEEP_TIME)
     
     def forward(self):
         self.PWM.setMotorModel(600,600,600,600)
-        time.sleep(1)
+        time.sleep(SLEEP_TIME)
     
     def right(self):
         self.PWM.setMotorModel(600,600,-600,-600)
-        time.sleep(1)
+        time.sleep(SLEEP_TIME)
     
     def left(self):
         self.PWM.setMotorModel(-600,-600,600,600)
-        time.sleep(1)
+        time.sleep(SLEEP_TIME)
     
     def stop(self):
         self.PWM.setMotorModel(0, 0, 0, 0)
