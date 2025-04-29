b70fb8fc7c31168485f6c18fe62062568a4015ef
jose-izarra <jaiza0912@gmail.com>
2025-04-29 19:58:20 +0200

    ultrasonic starting point changed

--- Code/Server/maze_solver.py
+++ Code/Server/maze_solver.py
@@ -71,25 +71,24 @@ class Car:
         if (time.time() - self.car_record_time) > 0.2:
             self.car_record_time = time.time()
             self.servo.set_servo_pwm('0', self.car_sonic_servo_angle)
-            if self.car_sonic_servo_angle == 30:
-                self.car_sonic_distance[0] = self.sonic.get_distance()
+            if self.car_sonic_servo_angle == 150:
+                self.car_sonic_distance[0] = self.sonic.get_distance()  # Left
             elif self.car_sonic_servo_angle == 90:
-                self.car_sonic_distance[1] = self.sonic.get_distance()
-            elif self.car_sonic_servo_angle == 150:
-                self.car_sonic_distance[2] = self.sonic.get_distance()
+                self.car_sonic_distance[1] = self.sonic.get_distance()  # Forward
+            elif self.car_sonic_servo_angle == 30:
+                self.car_sonic_distance[2] = self.sonic.get_distance()  # Right
 
             print("L:{}, M:{}, R:{}".format(self.car_sonic_distance[0], self.car_sonic_distance[1], self.car_sonic_distance[2]))
             self.run_motor_ultrasonic(self.car_sonic_distance)
 
-            if self.car_sonic_servo_angle <= 30:
-                self.car_sonic_servo_dir = 1
-            elif self.car_sonic_servo_angle >= 150:
+            if self.car_sonic_servo_angle >= 150:
                 self.car_sonic_servo_dir = 0
+            elif self.car_sonic_servo_angle <= 30:
+                self.car_sonic_servo_dir = 1
             if self.car_sonic_servo_dir == 1:
                 self.car_sonic_servo_angle += 60
             elif self.car_sonic_servo_dir == 0:
                 self.car_sonic_servo_angle -= 60
-
     def mode_infrared(self):
         if (time.time() - self.car_record_time) > 0.2:
             self.car_record_time = time.time()
